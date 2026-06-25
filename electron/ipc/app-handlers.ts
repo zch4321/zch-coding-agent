1a458df4be4b2841fc58314e4b6e126be6575440
zch <zch15384@gmail.com>
2026-06-25 12:03:56 +0800

    Revert "refactor(agent): fix real-api test script path and format touched files"
    This reverts commit d87c6da20de944c875b09aef4379934d1c2a8558.

--- electron/ipc/app-handlers.ts
+++ electron/ipc/app-handlers.ts
@@ -1,10 +1,7 @@
 import { BrowserWindow, dialog, shell, type OpenDialogOptions } from 'electron'
 import { stat } from 'node:fs/promises'
 import { TRACE_NOTICE_VERSION } from '../../shared/notices'
-import {
-  ChangeHistoryError,
-  ChangeHistoryStore,
-} from '../session/change-history'
+import { ChangeHistoryError, ChangeHistoryStore } from '../session/change-history'
 import {
   fetchOpenAICompatibleModelCatalog,
   ModelCatalogError,
